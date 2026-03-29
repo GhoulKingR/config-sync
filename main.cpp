52d06db0f4aa5280f620c97cf34f18057d70f2a2
Chigozie Oduah <oduahchigozie46@gmail.com>
2026-03-29 11:12:37 +0100

    top-level error handling

--- main.cpp
+++ main.cpp
@@ -1,54 +1,59 @@
 #include <cstdlib>
 #include <exception>
-#include <string>
+#include <iostream>
+#include <stdexcept>
 
 #include "defines.hpp"
 
 int main(int argc, const char **argv) {
-    Shell shell;
-    Configs opts(argc, argv, shell);
-
-    // Load options into shell then load config files
-    // The order matters here
-    shell.load_config(opts);
-    opts.load_config_file();
-    
-    const Logger logger("MAIN", opts.level);
-    const Application app(opts, shell);
-
     try {
-        switch (opts.command) {
-            case INIT_LOCAL:
-                app.init_local();
-                break;
-            case INIT_REMOTE:
-                app.init_remote();
-                break;
-            case PUSH_REMOTE:
-                app.push_remote();
-                break;
-            case PULL_REMOTE:
-                app.pull_remote();
-                break;
-            case EXPORT_ZIP:
-                app.export_zip();
-                break;
-            case IMPORT_ZIP:
-                app.import_zip();
-                break;
-            case CLONE_REMOTE:
-                app.clone_remote();
-                break;
-            case STATUS:
-                app.print_status();
-                break;
-            case HELP:
-            default:
-                app.print_help();
-                break;
+        Shell shell;
+        Configs opts(argc, argv, shell);
+
+        // Load options into shell then load config files
+        // The order matters here
+        shell.load_config(opts);
+        opts.load_config_file();
+        
+        const Logger logger("MAIN", opts.level);
+        const Application app(opts, shell);
+
+        try {
+            switch (opts.command) {
+                case INIT_LOCAL:
+                    app.init_local();
+                    break;
+                case INIT_REMOTE:
+                    app.init_remote();
+                    break;
+                case PUSH_REMOTE:
+                    app.push_remote();
+                    break;
+                case PULL_REMOTE:
+                    app.pull_remote();
+                    break;
+                case EXPORT_ZIP:
+                    app.export_zip();
+                    break;
+                case IMPORT_ZIP:
+                    app.import_zip();
+                    break;
+                case CLONE_REMOTE:
+                    app.clone_remote();
+                    break;
+                case STATUS:
+                    app.print_status();
+                    break;
+                case HELP:
+                default:
+                    app.print_help();
+                    break;
+            }
+        } catch (const std::runtime_error &e) {
+            logger.error(e.what());
         }
     } catch (const std::exception &e) {
-        logger.error(e.what());
+        std::cerr << "Error: " << e.what() << std::endl;
     }
 
     return 0;
